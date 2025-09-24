5fbe893746d9d92b9d01373bd272f1087459db40
Hellotagvg <138308471+Hellotagvg@users.noreply.github.com>
2025-09-24 09:06:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import pybybit as pb
 def main(identifier,price_a,price_b,symbol,type):
     max_wait = int()
     key_dict = {
-    "acc1": {"api_key": "OAO6GrRXfCTOnrSAtF", "Ct8vw1yVfQi1zEGRI67JKqxhoK5cKG1Xg2Pp": "SECRET1"},
-    "acc2": {"api_key": "API2", "api_secret": "SECRET2"}
-        } 
+    "acc1": {
+        "api_key": "OoRAfYzPq43Fgs3LM3",
+        "api_secret": "5MOxtE17mWBK3KqZZSoesXIoq5os7C1WEHHP"
+    }
+}
+
+
+
     dict_a,dict_b = c.tcl_calc(price_a,price_b,symbol,type)
-    pb.trade_tcl(key_dict,dict_a,dict_b,demotrading = True, max_wait_seconds = max_wait )
+    pb.trade_tcl(key_dict,dict_a,dict_b, max_wait_seconds = 6000)
 
 
-td_email = 'alerts@'
-em.start_email_listener(td_email,callback = main,poll_interval=3)
+td_email = 'hellotagvg1@gmail.com'
+em.start_spam_email_listener_recent(td_email,callback = main,poll_interval=3)
 
     
 
